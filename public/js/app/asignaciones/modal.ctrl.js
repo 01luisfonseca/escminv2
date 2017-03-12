221a57a3488b464f6181fd47cd5ceb928d274109
Luis Andres Fonseca Gomez <Luis Andres Fonseca Gomez>
2017-03-12 08:44:28 -0500

    Ajustes de presnetación de Alumnos

--- public/js/app/asignaciones/modal.ctrl.js
+++ public/js/app/asignaciones/modal.ctrl.js
@@ -9,6 +9,7 @@
 
 
 	'use strict';
+
 	angular.module('app.inicial')
     .controller('modalAsigEstCtrl', controller);
     
@@ -39,41 +40,6 @@
         function selAlumno(idx){
             console.log(vm.alumnos[idx]);
         }
-        // Devuelve el texto de ultimoTipo
-        function ultimoTipo(id){
-            for (var i = 0; i < vm.alumnos.length; i++) {
-                if (vm.alumnos[i].id==id) {
-                    if (vm.alumnos[i].asignaciones.length) {
-                        vm.alumnos[i].asignaciones.sort(function(a,b){
-                            // Turn your strings into dates, and then subtract them
-                            // to get a value that is either negative, positive, or zero.
-                            return new Date(b.updated_at) - new Date(a.updated_at);
-                        });
-                        return vm.alumnos[i].asignaciones[0].type;
-                    }else{
-                        return 'No tiene.';
-                    }
-                }
-            }
-        }
-
-        // Devuelve el texto de ultimoDiscurso
-        function ultimoDiscurso(id){
-            for (var i = 0; i < vm.alumnos.length; i++) {
-                if (vm.alumnos[i].id==id) {
-                    if (vm.alumnos[i].asignaciones.length) {
-                        vm.alumnos[i].asignaciones.sort(function(a,b){
-                            // Turn your strings into dates, and then subtract them
-                            // to get a value that is either negative, positive, or zero.
-                            return new Date(b.updated_at) - new Date(a.updated_at);
-                        });
-                        return vm.alumnos[i].asignaciones[0].updated_at;
-                    }else{
-                        return 'No tiene.';
-                    }
-                }
-            }
-        }
 
         // Obtener todos los alumnos
         function getAlumnos(genero){
@@ -81,7 +47,20 @@
             if (!genero) genero='ambos';
             return EstudiantesFct.asignable({genero:genero},(data)=>{
                 vm.alumnos=data;
-            })
+                vm.alumnos.sort((a,b)=>{
+                    if(a.name>b.name) return 1;
+                    return -1;
+                });
+                for (var i = 0; i < vm.alumnos[i].asignaciones.length; i++) {
+                   vm.alumnos[i].asignaciones.sort(function(a,b){
+                        return new Date(b.updated_at) - new Date(a.updated_at);
+                    });
+                    for (var j = 0; j < vm.alumnos[i].asignaciones.length; j++) {
+                        vm.alumnos[i].asignaciones[j].updated_at = new Date(vm.alumnos[i].asignaciones[j].updated_at);
+                        vm.alumnos[i].asignaciones[j].created_at = new Date(vm.alumnos[i].asignaciones[j].created_at);
+                    }
+                }
+            });
         }
          function ok() {
             $uibModalInstance.close('Cerrado con OK');
